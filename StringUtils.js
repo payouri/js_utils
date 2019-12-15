164e260257bc56be015c98b1fd76312a0d20f15b
Pailloux Youri <pailloux.youri@gmail.com>
2019-12-15 14:03:06 +0000

    add nFormatter

--- StringUtils.js
+++ StringUtils.js
@@ -61,6 +61,28 @@ export const secsToMin = d => {
     return ret;
 
 }
+export const nFormatter = (num, digits = 0) => {
+    const si = [
+        { value: 1, symbol: "" },
+        { value: 1E3, symbol: "k" },
+        { value: 1E6, symbol: "M" },
+        { value: 1E9, symbol: "G" },
+        { value: 1E12, symbol: "T" },
+        { value: 1E15, symbol: "P" },
+        { value: 1E18, symbol: "E" }
+    ];
+    const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+    let i;
+    for (i = si.length - 1; i > 0; i--) {
+        if (num >= si[i].value) {
+            break;
+        }
+    }
+    return (num / si[i].value).toFixed(digits).replace(rx, "$1") + si[i].symbol;
+}
+export const encodeHTML = s => {
+    return typeof s == 'string' ? s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;') : s;
+}
 
 export default {
     capitalize,
@@ -70,4 +92,6 @@ export default {
     secsToMin,
     stringToFunction,
     unCapitalize,
+    nFormatter,
+    encodeHTML,
 }
